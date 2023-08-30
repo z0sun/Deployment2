ea8e3baff72759f84122548d1966f5aab95cabaa
Joseph White <135557197+Joopwhite@users.noreply.github.com>
2023-08-30 16:51:41 -0400

    Update BashScript1.sh

--- BashScript1.sh
+++ BashScript1.sh
@@ -1,9 +1,24 @@
-#!/bin/bash
+!/bin/bash
 
 #This is an Update Script
  
 #By Joseph White 08/28/23
 
+#Update Server
+
+sudo apt update && sudo apt upgrade
+ 
 #Create file containing the list of upgradable packages
 
 sudo apt update > 08.30.23.txt
+
+#Notify task is complete
+
+echo "Task Completed!"
+
+#To set my cronjob
+
+crontab -e 
+0 23 * * 5 /home/ubuntu/Fridayjob.sh
+
+#I was a little confused about setting permissions but I guessed since I was not getting permissions errors I must have the correct privileges.
