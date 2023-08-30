4c0147d4d721e9661e31332bbce3f6a89a61f738
Joseph White <135557197+Joopwhite@users.noreply.github.com>
2023-08-30 16:23:15 -0400

    Update BashScript1.sh

--- BashScript1.sh
+++ BashScript1.sh
@@ -4,22 +4,6 @@
  
 #By Joseph White 08/28/23
 
-# Check if today is Friday and the time is 11 pm
-if [ "$(date +\%A)" == "Friday" ] && [ "$(date +\%H)" == "23" ]; then
-    # Update the server
-    sudo apt-get update && sudo apt-get upgrade -y
+#Create file containing the list of upgradable packages
 
-    # Get the number of upgradable packages
-    upgradable_packages=$(apt list --upgradable 2>/dev/null | wc -l)
-
-    # Create the filename with the current date
-    filename="update$(date +\%m.\%d.\%y).txt"
-
-    # Write the number of upgradable packages to the file
-    echo "Number of upgradable packages: $upgradable_packages" > "$filename"
-
-    echo "Task completed!"
-else
-    echo "Today is not Friday at 11 pm. Exiting."
-
-#Most of this was written with the help of chatGPT. I don't quite have a grasp on substitutions, wildcards, etc.
+sudo apt update > 08.30.23.txt
